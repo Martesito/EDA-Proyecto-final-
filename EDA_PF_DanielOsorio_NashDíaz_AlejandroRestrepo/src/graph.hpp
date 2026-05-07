6a141a0254603a65f282a517517f4b7d675bd277
Nash <nashvalentina@gmail.com>
2026-05-07 12:34:41 -0500

    Update graph.hpp

--- EDA_PF_DanielOsorio_NashDíaz_AlejandroRestrepo/src/graph.hpp
+++ EDA_PF_DanielOsorio_NashDíaz_AlejandroRestrepo/src/graph.hpp
@@ -17,4 +17,22 @@ private:
     int n_nodes;
     long long n_edges;
 
+public:
+    Graph();
+
+    int get_or_create(int original_id);
+    int find_internal(int original_id) const;
+    int get_original(int internal_id) const;
+
+    void add_undirected_edge(int u_internal, int v_internal, int weight);
+
+    const std::vector<Edge>& neighbors(int u_internal) const;
+
+    int num_nodes() const;
+    long long num_edges() const;
+
+    void reserve_nodes(int n);
+};
+
+#endif
 
